506947243269acaef3500577392036339cc65515
fodderebecca <33453472+fodderebecca@users.noreply.github.com>
2017-11-13 17:34:34 +0100

    Add files via upload

---
+++ Ordinamento 2 array thread (GitHub)/Ordinamento 2 array thread (GitHub)/Ordinamento 2 array thread/Fodde.cpp
@@ -0,0 +1,23 @@
+unsigned int _stdcall FoddeF(void *data)
+{
+	int *V = (int*)data;
+	bool cambio;
+
+	do
+	{
+		cambio = false;
+		for (int i = 0; i < 99 - 1; i++)
+		{
+			if (V[i + 1] < V[i])
+			{
+				int x = V[i + 1];
+				V[i + 1] = V[i];
+				V[i] = x;
+
+				cambio = true;
+			}
+		}
+
+	} while (cambio == true);
+	return 0;
+}
