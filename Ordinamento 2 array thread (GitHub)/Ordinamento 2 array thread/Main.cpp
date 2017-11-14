46968d820ab95a4bf11e9681a31850428c7a24f0
Pandolfini-Marco <33453412+Pandolfini-Marco@users.noreply.github.com>
2017-11-14 11:48:05 +0100

    modifica fatta da Pandolfini

--- Ordinamento 2 array thread (GitHub)/Ordinamento 2 array thread/Main.cpp
+++ Ordinamento 2 array thread (GitHub)/Ordinamento 2 array thread/Main.cpp
@@ -2,6 +2,7 @@
 @author Fiordi Stefano
 @date 21/09/2017
 Ordinamento di due vettori dichiarati nel main con 2 thread utilizzando un'unica funzione
+ Ma Ã¨ proprio un bel programmino
 */
 
 #include<iostream>
@@ -92,8 +93,8 @@ void main()
 		iPoneti2[i] = rand() % 1000 + 1;
 	}
 
-	/// Per lanciare i thread uso _beginthreadex, e non _beginthread, perché quest'ultimo quando l'esecuzione del thread
-	/// viene terminata velocemente, la funzione si comporta in modo anomalo, e non è più possibile chiudere l'handle del relativo thread
+	/// Per lanciare i thread uso _beginthreadex, e non _beginthread, perchÃ© quest'ultimo quando l'esecuzione del thread
+	/// viene terminata velocemente, la funzione si comporta in modo anomalo, e non Ã¨ piÃ¹ possibile chiudere l'handle del relativo thread
 	HANDLE hOrd1 = (HANDLE)_beginthreadex(NULL, 0, &thOrdFiordi, iV, 0, &threadid); /// Lancio del primo thred
 	HANDLE hOrd2 = (HANDLE)_beginthreadex(NULL, 0, &thOrdFiordi, iW, 0, &threadid); /// Lancio del secondo thread
 
